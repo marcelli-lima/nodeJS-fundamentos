76a5ea778c25ad0bb8521f2bd8369c20c59cfe5c
marcelli-lima <marcelli_lima@hotmail.com>
2023-02-01 10:26:21 -0300

    salvando informaçoes num banco de dados fake

--- src/server.js
+++ src/server.js
@@ -1,22 +1,30 @@
 import http from "node:http";
+import { Database } from "./database.js";
+import { json } from "./middlewares/json.js";
 
-const users = [];
+const database = new Database();
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
+  await json(req, res);
+
   if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+    const users = database.select("users");
+
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    users.push({
+    const { name, email } = req.body;
+
+    const user = {
       id: 1,
-      name: "Marcelli Lima",
-      email: "marcelli_lima@hotmail.com",
-    });
+      name,
+      email,
+    };
+
+    database.insert("users", user);
     return res.writeHead(201).end();
   }
 
